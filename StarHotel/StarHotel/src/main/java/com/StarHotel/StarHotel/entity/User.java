25a2502925f94ff64d29b1a89aed0750418baf30
d-sar <damrisara1@gmail.com>
2025-09-26 18:27:36 +0100

    first commit

---
+++ StarHotel/StarHotel/src/main/java/com/StarHotel/StarHotel/entity/User.java
@@ -0,0 +1,18 @@
+package com.StarHotel.StarHotel.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name ="user")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+
+
+  
+
+}
