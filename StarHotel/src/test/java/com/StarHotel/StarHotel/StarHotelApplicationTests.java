c7cafba7984769ef6d1fa13baa676e2ce0668a84
d-sar <damrisara1@gmail.com>
2025-09-26 18:30:51 +0100

    first commit

---
+++ StarHotel/src/test/java/com/StarHotel/StarHotel/StarHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StarHotel.StarHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
